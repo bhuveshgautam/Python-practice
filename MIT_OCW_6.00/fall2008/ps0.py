3182a5e5e072f5c669290d776110c7035e2b5317
doubleplusc <changli@usc.edu>
2015-12-22 14:35:10 -0800

    Starting with fall2008 problems. Finished ps0. Finished problem 1 on ps1. TODO: problem 2 on ps1.

---
+++ MIT_OCW_6.00/fall2008/ps0.py
@@ -0,0 +1,11 @@
+'''Write a program that does the following in order:
+1. Asks the user to enter his/her last name.
+2. Asks the user to enter his/her first name.
+3. Prints out the user's first and last names in that order.
+'''
+
+last = input('Enter your last name: ')
+
+first = input('Enter your first name: ')
+
+print('Your name is {} {}'.format(first, last))
---
+++ MIT_OCW_6.00/fall2008/ps1.py
@@ -0,0 +1,33 @@
+'''Computing Prime numbers'''
+
+import math  # http://stackoverflow.com/questions/9439480/from-import-vs-import
+
+prime_ordinal = 1  # since 2 is the first prime but not generated, we already have a prime to start with
+
+prime_ordinal_des = int(input('Enter the Xth prime number you want to find: '))  # input() gives a string, remember to convert to int!!
+
+test_val = 2  # [1]
+
+
+while prime_ordinal < prime_ordinal_des:
+
+	is_prime = True  # starting value of prime value is true, will change to false when checking primality
+	test_val += 1  # iterate to check the next number for primality
+	#  [1]
+
+	for x in range(2, math.ceil(math.sqrt(test_val)) + 1):  # the maximum possible factor is for x^2 = x * x. So use sqrt() and round up (range does not include end number) to find possible range
+		if(test_val % x) == 0:  # number is divisible by current iteration of x, immediately know it is not prime
+			is_prime = False
+			break  # check the next iteration
+
+	if is_prime:
+		prime_ordinal += 1
+
+print('The prime at position {} is {}'.format(prime_ordinal_des, test_val))  # [1]
+
+# NOTES
+'''
+[1]:
+Originally, test_val starts at 3 and was incremented at the end of the loop. The problem with this is that at the print statement, test_val would be +OBO.
+So instead of doing test_val-1 for the print statement at the end, just rearrange starting test_val to 2, and increment at the beginning of the loop. Saves an operation.
+'''
