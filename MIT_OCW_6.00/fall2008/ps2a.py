ffcd3a4e7bbaa350cbbf1c8be451fece5eb7955e
doubleplusc <changli@usc.edu>
2015-12-25 23:26:31 -0800

    Added more pseudocode for bounds checking

--- MIT_OCW_6.00/fall2008/ps2a.py
+++ MIT_OCW_6.00/fall2008/ps2a.py
@@ -7,26 +7,31 @@ start_nuggets = 1  # technically could be the smallest multiple of nuggets possi
 current_nuggets = start_nuggets
 
 # 4a + 9b + 20c = n
-# [1] set upper bound on how many max packs are possible in permutations
-a = current_nuggets / 6
-b = current_nuggets / 9
-c = current_nuggets / 20
+# [1] set upper bound on how many max packs are possible in permutations. Need ceiling for max to work with range()
+a = math.ceil(current_nuggets / 6)
+b = math.ceil(current_nuggets / 9)
+c = math.ceil(current_nuggets / 20)
 
 
 def check_nuggets():
+	# check bounds
+		# sum of smallest buyable quantity must be smaller than tested value
+		# otherwise, increment until it is, then loop through the nugget buying
+		# kind of dumb if numbers are hard coded, but useful for next problem, when numbers are variable
+
+
 	while numConsecutive < 6:  # from problem 2, we know for a 6pack, we need 6 consecutive values to get every possible value after
 		sum_nuggets()  # [2] http://stackoverflow.com/questions/189645/how-to-break-out-of-multiple-loops-in-python
 		
 
 def sum_nuggets():
 	# [2] why use separate function?
-	# for range in a
-		# for range in b
-			# for range in c
-				# check if Diophantine equation is true. 
+	for x in range(1, a):
+		for y in range(1, b):
+			for z in range(1,c):
+				if(x*6 + y*9 + z*20 == current_nuggets) 
 				# if true, increment current_nuggets, RETURN true
 	# if no match is found after exhausting all combinations, increment num_consecutive, RETURN false
-
 
 
 '''Notes
