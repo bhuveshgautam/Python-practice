a057e19895157d60e0664e88fb5fc20d7ef38fa2
doubleplusc <changli@usc.edu>
2015-12-26 22:45:41 -0800

    Confused the program design! Rewrote pseudocode, implemented functions Left is debug

--- MIT_OCW_6.00/fall2008/ps2a.py
+++ MIT_OCW_6.00/fall2008/ps2a.py
@@ -5,6 +5,7 @@ import math
 num_consecutive = 0  # number of consecutive nuggets that could be bought
 start_nuggets = 1  # technically could be the smallest multiple of nuggets possible, but problem states to start 1
 current_nuggets = start_nuggets
+last_unbuyable = start_nuggets
 
 # 4a + 9b + 20c = n
 # [1] set upper bound on how many max packs are possible in permutations. Need ceiling for max to work with range()
@@ -18,20 +19,30 @@ def check_nuggets():
 		# sum of smallest buyable quantity must be smaller than tested value
 		# otherwise, increment until it is, then loop through the nugget buying
 		# kind of dumb if numbers are hard coded, but useful for next problem, when numbers are variable
+	while(current_nuggets < 6):
+		last_unbuyable = current_nuggets
+		current_nuggets += 1
 
 
-	while numConsecutive < 6:  # from problem 2, we know for a 6pack, we need 6 consecutive values to get every possible value after
+	while num_consecutive < 6:  # from problem 2, we know for a 6pack, we need 6 consecutive values to get every possible value after
 		sum_nuggets()  # [2] http://stackoverflow.com/questions/189645/how-to-break-out-of-multiple-loops-in-python
-		
+	
+	print("Largest number of McNuggets that cannot be bought in exact quantity: {}".format(last_unbuyable))
 
 def sum_nuggets():
 	# [2] why use separate function?
 	for x in range(1, a):
 		for y in range(1, b):
 			for z in range(1,c):
-				if(x*6 + y*9 + z*20 == current_nuggets) 
-				# if true, increment current_nuggets, RETURN true
-	# if no match is found after exhausting all combinations, increment num_consecutive, RETURN false
+				if(x*6 + y*9 + z*20 == current_nuggets):
+				# if true, increment current_nuggets and num_consecutive, return
+					current_nuggets += 1
+					num_consecutive += 1
+					return
+	# if no match is found after exhausting all combinations, clear num_consecutive, update last_unbuyable, return
+	last_unbuyable = current_nuggets
+	num_consecutive = 0
+	return
 
 
 '''Notes
