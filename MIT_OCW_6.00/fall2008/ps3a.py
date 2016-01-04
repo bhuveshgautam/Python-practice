86ce0eebbfca7c75a1b04035f2ff1eb746dfca0e
doubleplusc <changli@usc.edu>
2016-01-03 23:51:31 -0800

    Problem 2 in ps3 returns the correct result, but I need to figure out how to make the base case print the output only once. If I put the print statement in the key_index == -1 statement, it is at the last recursion frame and will have no information about the topmost frame. AHHHH. Perhaps switch the order of the conditions? Put the match found first, and then the not found.

--- MIT_OCW_6.00/fall2008/ps3a.py
+++ MIT_OCW_6.00/fall2008/ps3a.py
@@ -1,5 +1,6 @@
 from string import *
 
+
 def countSubStringMatch(target, key):
 
     key_instances = 0
@@ -12,15 +13,27 @@ def countSubStringMatch(target, key):
         key_index = target.find(key, key_index + 1)
         key_instances += 1
 
-    print("""The string "{}" appears in {} {} times. """.format(key, target, key_instances))
+    print("""The string "{}" appears in "{}" {} times. """.format(key, target, key_instances))
 
 
 def countSubStringMatchRecursive(target, key):
     
-    key_instances = 0
+    # since a counter is not supplied within the arguments, solution is to call another function that passes a counter (but that seems cheap)
+    # other solution is to return an int... but how to make sure it doesn't reset to 0 every time...?
+
+    key_index = target.find(key)
+
+    if key_index == -1:  # base case, can leave
+        key_instances = 0
+        return key_instances
+    else:  # match found, increment
+        key_instances = 1  # add one for this recursion frame
+        key_instances += countSubStringMatchRecursive(target[key_index + 1:], key)
+        print("""The string "{}" appears in "{}" {} times. """.format(key, target, key_instances))
+        return key_instances
 
 
 if __name__ == '__main__':
     target = "atgacatgcacaagtatgcat"
     key = "atgc"
-    countSubStringMatch(target, key)
+    countSubStringMatchRecursive(target, key)
