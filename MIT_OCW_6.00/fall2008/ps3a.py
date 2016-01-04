d8196d0452137409c6044c7410ef3d822c9ff2d1
doubleplusc <changli@usc.edu>
2016-01-03 23:07:08 -0800

    Problem 1 in ps3 done.
    Recursion reading done. Need to make -1 in .find() base case. Recursive
    call sends target string with start index of last .find() value + 1.

--- MIT_OCW_6.00/fall2008/ps3a.py
+++ MIT_OCW_6.00/fall2008/ps3a.py
@@ -6,9 +6,21 @@ def countSubStringMatch(target, key):
 
     #find the first appearance
     #find the next appearance, with the last found appearance's index + 1. (Can't do last index + len(key) for repeating strings like finding "hihi" in "hihihi")
+    key_index = target.find(key)
 
+    while key_index != -1:
+        key_index = target.find(key, key_index + 1)
+        key_instances += 1
+
+    print("""The string "{}" appears in {} {} times. """.format(key, target, key_instances))
 
 
 def countSubStringMatchRecursive(target, key):
     
     key_instances = 0
+
+
+if __name__ == '__main__':
+    target = "atgacatgcacaagtatgcat"
+    key = "atgc"
+    countSubStringMatch(target, key)
